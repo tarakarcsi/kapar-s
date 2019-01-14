f0a0259bed11825d9d5ef311b2e0ebba67230dcb
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-01-14 15:01:06 +0100

    all

--- Driver.java
+++ Driver.java
@@ -3,16 +3,14 @@ import java.util.Random;
 public class Driver extends Main {
 
     private String name;
-    private int position;
     private double skill;
     private double luck;
 
-    public Driver(double position, double skill, double luck) {
+    public Driver(double skill, double luck) {
         String[] nameList = {"Májkül Sumaher", "Lújisz Hemilton", "Szebásztián Fettel", "Kimi Rájkőnen", "Dzsenzon Bátön", 
             "Fernándó Álonzó", "Rúbensz Bárikelló", "Szebásztián Lőb", "Máksz Fersztáppen", "Kolin Mekrí"};
         Random r = new Random();
         name = nameList[r.nextInt(nameList.length)];
-        this.position = 1 + (10 - 0) * r.nextInt();
         this.skill = 1 + (10 - 0) * r.nextDouble();
         this.luck = 1 + (10 - 0) * r.nextDouble();
     }
@@ -20,11 +18,7 @@ public class Driver extends Main {
     public String getName() {
         return name;
     }
-
-    public double getPostion() {
-        return position;
-    }
-
+    
     public double getSkill() {
         return skill;
     }
@@ -32,5 +26,4 @@ public class Driver extends Main {
     public double getLuck() {
         return luck;
     }
-
 }
