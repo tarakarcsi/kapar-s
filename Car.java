6f3d073d4afa67aa2822bede6214147fdcf3ef38
FreePeterOrliczki <peterorliczki2@gmail.com>
2019-01-14 14:06:33 +0100

    brand names

--- Car.java
+++ Car.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Car {
 
-    
+    private String brand;
     private int position;
     private double horsePower;
     private double zeroToHundred;
@@ -16,12 +16,18 @@ public class Car {
     // }
 
     public Car(double position, double horsePower, double zeroToHundred, double weight, double luck) {
+        String[] brandList = {"Aszton Martin", "Béjemvé", "Dodzs", "Kilya", "Mázerátti", "Dzsipp", "Kőnigszeg", "Micubicsi", "Krájzler", "Bentli"};
         Random r = new Random();
+        brand = brandList[r.nextInt(brandList.length)];
         this.position = 1 + (10 - 0) * r.nextInt();
         this.horsePower = 1 + (10 - 0) * r.nextDouble();
         this.zeroToHundred = 1 + (10 - 0) * r.nextDouble();
         this.weight = 1 + (10 - 0) * r.nextDouble();
         this.luck = 1 + (10 - 0) * r.nextDouble();
+    }
+
+    public String getBrand() {
+        return brand;
     }
 
     public double getPosition() {
